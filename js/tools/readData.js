855dfa06842d1ef6d3ebfaf4213cc3dee198ce7d
Stormfish <love.ekstam@gmail.com>
2015-03-04 17:00:42 +0100

    commented readData code to avoid js fail

--- js/tools/readData.js
+++ js/tools/readData.js
@@ -1,10 +1,10 @@
 define(["jquery","d3","topojson","datamaps", "jqueryui","data/util","data/migration"],
 	function($,d3){
 		var _1960; 
-		d3.csv("../rawdata/1960.csv", function(data) {
+		/*d3.csv("../rawdata/1960.csv", function(data) {
         	_1960=data;
         	console.log(_1960);
-        });
+        });*/
 
 		console.log("readData");
 	});
