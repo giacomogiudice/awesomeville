6a5cf6d445f55f81bb66febaa091cb7f0845d566
Stormfish <love.ekstam@gmail.com>
2015-03-05 13:32:24 +0100

    merge

--- js/tools/readData.js
+++ js/tools/readData.js
@@ -1,10 +1,18 @@
 define(["jquery","d3","topojson","datamaps", "jqueryui","data/util","data/migration"],
 	function($,d3){
-		var _1960; 
-		/*d3.csv("../rawdata/1960.csv", function(data) {
-        	_1960=data;
-        	console.log(_1960);
-        });*/
+		//TODO: replace with relative link when we are that far.
+		var dataByYear=[];
+		var firstYear = 1960; 
+		var lastYear = 2000; 
+		var interval = 10;
 
-		console.log("readData");
+		for(var i = firstYear; i <= lastYear; i+= interval){
+        	var yearString = i.toString();
+        	console.log(yearString);
+
+			d3.csv("http://giacomogiudice.github.io/awesomeville/rawdata/"+yearString+".csv", function(data) {
+	        	//dataByYear[yearString]=data;
+	        	dataByYear.push(data);
+        	});
+		}
 	});
