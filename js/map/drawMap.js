1ffa8a61798065987bc1e3d86780fc2d40db921d
Giacomo Giudice <giacomo.giudice@mail.com>
2015-03-05 13:18:34 +0100

    Refactoring code
    - Directly get country id
    - Move onclick handling to updateMap
    - renaming rawdata to raw

--- js/map/drawMap.js
+++ js/map/drawMap.js
@@ -1,5 +1,5 @@
-define(["jquery","d3","topojson","datamaps", "jqueryui","data/util","data/migration"],
-	function($,d3,topojson,Datamap,jqueryui,util,migration) {
+define(["jquery","d3","topojson","datamaps", "jqueryui","map/updateMap"],
+	function($,d3,topojson,Datamap,jqueryui,updateMap) {
 
     /* This is how you would bind an event to the slider from here,
     NOTE: you have to remove the event bound when the slider is created,
@@ -32,95 +32,12 @@ define(["jquery","d3","topojson","datamaps", "jqueryui","data/util","data/migrat
 	}));*/
 
     //TODO: Sane coloring, remove row argument, replace with better solution
-    function addArc(array, origin, destination, row){
-        array.push({
-            "origin": {
-                "latitude": util.positions[origin][0],
-                "longitude": util.positions[origin][1],
-            },
-            "destination": {
-                "latitude": util.positions[destination][0],
-                "longitude": util.positions[destination][1],
-            },
-            "options": {
-                "strokeWidth": lineWidth(migration[global.year][row][i]),
-                "strokeColor": strokeColor(origin, destination, migration[global.year][row][i])
-            }
-        });
-    };
-
-    //Decide how wide to draw a line
-    function lineWidth(value){
-        return 2; 
-        //Old way: Math.log(migration[global.year][row][i]),
-    }
-    //Decide color of line on map
-    function strokeColor(origin, destination, value){
-        return '#'+Math.floor(Math.random()*16777215).toString(16);
-    }
-
+    
 	//add onlick event
 	map.svg.selectAll('.datamaps-subunit').on('click', function(geo) {
-        map.svg.selectAll('path.datamaps-arc').remove();
-        var row = "", code = "";
-        var arcs = []
-        global.country = geo.properties.name;
-        for(i in util.countrycodes) {
-            if(global.country === util.countrycodes[i][0]) {
-                code = util.countrycodes[i][1];
-                row = util.countryorder.indexOf(code);
-            }
-        }
-        if (row === "") {
-            if(Math.random() < 0.3 || $(window).width() > 2000) $("#lol").show().animate({left: "990"},4000,function(){$("#lol").hide().css({left: -320})});
-            else console.log(global.country + " not found");
-
-        }
-        else {
-            for(i in migration[global.year][row]) {
-                if(migration[global.year][row][i] ) {
-                    if(util.positions[util.countryorder[i]]) { //SUPER HACKY
-                        
-                        //TODO: better threshold for drawing line
-                        if(migration[global.year][row][i]>1000){
-                            addArc(arcs, code, util.countryorder[i], row);
-                        }
-
-                        /* Old line adding code, remove when properly replaced
-                        arcs.push({
-                            "origin": {
-                                "latitude": util.positions[code][0],
-                                "longitude": util.positions[code][1],
-                            },
-                            "destination": {
-                                "latitude": util.positions[util.countryorder[i]][0],
-                                "longitude": util.positions[util.countryorder[i]][1],
-                            },
-                            "options": {
-                                "strokeWidth": Math.log(migration[global.year][row][i]),
-                                "strokeColor": '#'+Math.floor(Math.random()*16777215).toString(16)
-                            }
-                        });*/
-                    }
-                } 
-
-            }
-            map.arc(arcs);
-        }
+        updateMap(geo);
     });
 
-    $("body").append('<img id="lol" src="img/omg-cat.gif" style="position:absolute;top:50px;left:-320px;z-index:+999"/>')
-    $("#lol").hide();
-    // if(geo.properties.name == 'Sweden'){
-
-    //   map.svg.selectAll('path.datamaps-arc').remove()
-    //   map.arc( sweLines, {strokeWidth: 2});                 
-    // }
-    // if(geo.properties.name == 'Norway'){
-    //   map.svg.selectAll('path.datamaps-arc').remove()
-    //   map.arc( norLines, {strokeWidth: 2});
-    // }  
-    // console.log(geo.properties.name);
     return map;
 
 });
--- js/map/updateMap.js
+++ js/map/updateMap.js
@@ -1,8 +1,53 @@
-define(["jquery"],function($) {
-	return function(map) {
-		var new_data = {};
-		// Populate new_data
+define(["jquery","data/util","data/migration"],function($,util,migration) {
+	var row = "", code = "";
+    var arcs = [];
+	function addArc(array, origin, destination, row){
+        array.push({
+            "origin": {
+                "latitude": util.positions[origin][0],
+                "longitude": util.positions[origin][1],
+            },
+            "destination": {
+                "latitude": util.positions[destination][0],
+                "longitude": util.positions[destination][1],
+            },
+            "options": {
+                "strokeWidth": lineWidth(migration[global.year][row][i]),
+                "strokeColor": strokeColor(origin, destination, migration[global.year][row][i]),
+            }
+        });
+    };
 
-		map.updateChoropleth(new_data);
+    //Decide how wide to draw a line
+    function lineWidth(value){
+        return 2; 
+        //Old way: Math.log(migration[global.year][row][i]),
+    }
+    //Decide color of line on map
+    function strokeColor(origin, destination, value){
+        function RGBComponent() {
+            return Math.round(Math.random() * 255);
+        }
+        return "rgba(" + RGBComponent() + "," + RGBComponent() + "," + RGBComponent() + ", 0.6)"; 
+        // return '#'+Math.floor(Math.random()*16777215).toString(16);
+    }
+
+	return function(geo) {
+		map.svg.selectAll('path.datamaps-arc').remove();
+		//prevent spurious calls
+		if(!global.map || typeof geo.properties === "undefined") { return }
+        global.country = geo.properties.name;
+        code = geo.id;
+        row = util.countryorder.indexOf(code);
+        if (row === -1) { console.log(geo.id + " not found") }
+        else {
+            for(i in migration[global.year][row]) {
+                if(migration[global.year][row][i] >= 1000) {
+                    //TODO: better threshold for drawing line
+                    addArc(arcs, code, util.countryorder[i], row);
+                }
+            }
+            global.map.arc(arcs);
+       } 
 	};
 });
