5652e9e82bfca6544436720bf581656a96396ab5
Giacomo Giudice <giacomo.giudice@mail.com>
2015-03-07 16:51:21 +0100

    Added bubbles
    Bubbles appear at random

--- js/map/updateMap.js
+++ js/map/updateMap.js
@@ -1,8 +1,8 @@
 define(["jquery","data/util","data/migration"],function($,util,migration) {
 	var row = "", code = "";
+    var arcs = [], bubbles = [], colors = [];
     
-	function addArc(array, origin, destination){
-
+	function addArc(array,origin, destination){
         var originCode = util.countryorder[origin];
         var destinationCode = util.countryorder[Number(destination)];
         array.push({
@@ -20,6 +20,16 @@ define(["jquery","data/util","data/migration"],function($,util,migration) {
             }
         });
     };
+
+    function addBubble(array,name,code,radius) {
+        array.push({
+            "name": name,
+            "latitude": util.positions[code][0],
+            "longitude": util.positions[code][1],
+            "radius": radius,
+            "fillKey": "default"
+        });
+    }
 
     //Decide how wide to draw a line
     function lineWidth(value){
@@ -46,7 +56,7 @@ define(["jquery","data/util","data/migration"],function($,util,migration) {
 
 	return function() {
 
-        var arcs = [];
+        
         var code = global.id;
 		global.map.svg.selectAll('path.datamaps-arc').remove();
 		//prevent spurious calls
@@ -54,11 +64,12 @@ define(["jquery","data/util","data/migration"],function($,util,migration) {
         row = util.countryorder.indexOf(code);
         if (row === -1) { console.log(code + " not found"); }
         else {
+            arcs = [];
             for(i in getDataByYear(global.year)){
 
                 //TODO: Add threshold function()
                 if(getDataByYear(global.year)[row][i]>=1000){
-                    addArc(arcs, row, i);
+                    addArc(arcs,row, i);
                 }
             }
             /* Old way
@@ -74,11 +85,18 @@ define(["jquery","data/util","data/migration"],function($,util,migration) {
        // handle contry colors
        // colors is the object mapping countrycode -> color
        // if fillkeys are used you can use it to assign a {fillKey: , value: }
-        var colors = [];
+        colors = [];
         for (i in util.countryorder) {
             colors[util.countryorder[i]] = '#'+Math.floor(Math.random()*16777215).toString(16);
         } 
         global.map.updateChoropleth(colors);
+        console.log(bubbles)
+        //handle bubbles;
+        bubbles = [];
+        for (i in util.countryorder) {
+            if(Math.random() <0.05) addBubble(bubbles,"",util.countryorder[i],Math.random()*50);
+        }
+        global.map.bubbles(bubbles);
 	};
 
 });
