da792a2bf8896293bfea5df8dc50da057f6d2dc9
Giacomo Giudice <giacomo.giudice@mail.com>
2015-03-13 02:06:14 +0100

    Compiling with r.js

--- js/tools/build.js
+++ js/tools/build.js
@@ -2,9 +2,11 @@
 	baseUrl: "../",
 	paths: {
 		"jquery": "empty:",
+		"jqueryui": "empty:",
 		"d3": "empty:",
 		"topojson": "empty:",
-		"datamaps": "empty:"
+		"datamaps": "empty:",
+		"readdata": "tools/readData"
 	},
 	name: "main",
 	out: "../main.min.js"
